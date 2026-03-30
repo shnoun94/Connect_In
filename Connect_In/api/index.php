d1d8482190f02984e611f72d64cf56f1806a8660
Shanna <shanna.elie@epitech.eu>
2026-03-30 16:01:17 +0200

    vercel

--- Connect_In/api/index.php
+++ Connect_In/api/index.php
@@ -1,7 +1,8 @@
 <?php
-echo "<pre>";
-echo "DIR: " . __DIR__ . "\n";
-echo "ROOT: " . dirname(__DIR__) . "\n";
-echo "FILES: ";
-print_r(scandir(dirname(__DIR__)));
-echo "</pre>";
+// echo "<pre>";
+// echo "DIR: " . __DIR__ . "\n";
+// echo "ROOT: " . dirname(__DIR__) . "\n";
+// echo "FILES: ";
+// print_r(scandir(dirname(__DIR__)));
+// echo "</pre>";
+require __DIR__ . "/../public/index.php";
