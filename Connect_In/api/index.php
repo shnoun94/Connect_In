6ef47f148024e5d4ec14bb7faf3788b673fc318f
Shanna <shanna.elie@epitech.eu>
2026-03-30 15:44:57 +0200

    debug paths

--- Connect_In/api/index.php
+++ Connect_In/api/index.php
@@ -1,10 +1,7 @@
 <?php
-define('LARAVEL_START', microtime(true));
-$root = dirname(__DIR__);
-require $root . '/vendor/autoload.php';
-$app = require_once $root . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-$kernel->terminate($request, $response);
+echo "<pre>";
+echo "DIR: " . __DIR__ . "\n";
+echo "ROOT: " . dirname(__DIR__) . "\n";
+echo "FILES: ";
+print_r(scandir(dirname(__DIR__)));
+echo "</pre>";
