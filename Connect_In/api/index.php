a1bc034d749184066687c317713163101eb6a772
Shanna <shanna.elie@epitech.eu>
2026-03-30 15:42:06 +0200

    fix: move vercel config to correct directory

--- Connect_In/api/index.php
+++ Connect_In/api/index.php
@@ -1,17 +1,10 @@
 <?php
-
 define('LARAVEL_START', microtime(true));
-
 $root = dirname(__DIR__);
-
 require $root . '/vendor/autoload.php';
-
 $app = require_once $root . '/bootstrap/app.php';
-
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 )->send();
-
 $kernel->terminate($request, $response);
